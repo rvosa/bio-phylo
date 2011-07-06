1b50b5adb176575c208568c7e6367b7f14b26ead
rvosa <rutgeraldo@gmail.com>
2011-07-06 17:47:01 +0100

    Added html serialization and redirect

--- lib/Bio/Phylo/PhyloWS/Service/Tolsearch.pm
+++ lib/Bio/Phylo/PhyloWS/Service/Tolsearch.pm
@@ -1,6 +1,16 @@
 package Bio::Phylo::PhyloWS::Service::Tolsearch;
 use base 'Bio::Phylo::PhyloWS::Service';
+use strict;
+use constant URL => 'http://tolweb.org/tree/home.pages/searchTOL?taxon=';
 
-sub get_supported_formats { ['nexml'] }
+sub get_supported_formats { [ 'nexml', 'html' ] }
+
+sub get_redirect {
+    my ( $self, $cgi ) = @_;
+    if ( $cgi->param('format') eq 'html' ) {
+        return URL . $cgi->param('query');
+    }
+    return;
+}
 
 1;
