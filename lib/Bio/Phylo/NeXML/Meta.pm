8222d280adb603dd964f3a641c569291a0706279
rvosa <rutgeraldo@gmail.com>
2011-07-07 19:28:52 +0100

    Other types of URIs should probably also be hrefs, so we do this now also for urn: pseudoprotocols

--- lib/Bio/Phylo/NeXML/Meta.pm
+++ lib/Bio/Phylo/NeXML/Meta.pm
@@ -79,7 +79,7 @@ as an element called 'meta', with RDFa compliant attributes.
         my %resource = ( 'xsi:type' => 'nex:ResourceMeta' );
         my %literal  = ( 'xsi:type' => 'nex:LiteralMeta' );
         if ( not ref $content ) {
-            if ( $content =~ m|^http://| ) {
+            if ( $content =~ m|^http://| or $content =~ m|^urn:| ) {
                 $self->set_attributes( 'href' => $content, %resource );
                 if ( my $prop = $self->get_attributes('property') ) {
                     $self->set_attributes( 'rel' => $prop );
