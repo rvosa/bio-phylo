d97dc5a4c7fa1dc612b4f32eba84bbb2ef563706
rvosa <rutgeraldo@gmail.com>
2011-07-12 16:42:58 +0100

    Oops, don't chop off that character

--- lib/Bio/Phylo/NeXML/Meta.pm
+++ lib/Bio/Phylo/NeXML/Meta.pm
@@ -80,7 +80,7 @@ as an element called 'meta', with RDFa compliant attributes.
         my %literal  = ( 'xsi:type' => 'nex:LiteralMeta' );
         if ( not ref $content ) {
             if ( $content =~ m|^http://| or $content =~ m|^urn:| ) {
-		$content =~ s/&[^a]/&amp;/g;
+		$content =~ s/&([^a])/&amp;$1/g;
                 $self->set_attributes( 'href' => $content, %resource );
                 if ( my $prop = $self->get_attributes('property') ) {
                     $self->set_attributes( 'rel' => $prop );
