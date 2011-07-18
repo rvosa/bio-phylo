5925565be176762b45d82e3353d8e8f27710534b
rvosa <rutgeraldo@gmail.com>
2011-07-18 16:34:47 +0100

    Stripping the prolog from the JSON, otherwise the parser chokes. Adding the default namespaces - though additional ones that should have been in the JSON can't be reconstructed unfortunately.

--- lib/Bio/Phylo/Parsers/Json.pm
+++ lib/Bio/Phylo/Parsers/Json.pm
@@ -13,7 +13,15 @@ Bio::Phylo::Parsers::Json - Parser used by Bio::Phylo::IO, no serviceable parts 
 =head1 DESCRIPTION
 
 This module is used to import NeXML data that was re-formatted as JSON, using
-the mapping implemented by L<XML::XML2JSON>.
+the mapping implemented by L<XML::XML2JSON>. This module is experimental in that
+complex NeXML-to-JSON mapped strings may fail to yield valid NeXML (and,
+consequently, valid Bio::Phylo objects) in the round trip. The reason for this
+is that the XML2JSON omits xmlns declarations in its JSON output. We try to work
+around this here by re-introducing the default namespaces, but if any additional
+ones were present in the original NeXML (e.g. to specify namspaces for metadata
+predicates) these can't be reconstructed. In addition, the JSON that XML2JSON
+produces doesn't preserve element order. We try to be lenient about this when
+parsing the intermediate NeXML, though it is actually invalid.
 
 =cut
 
@@ -21,8 +29,20 @@ sub _parse {
     my $self = shift;
     my $fh   = $self->_handle;
     my $json = do { local $/; <$fh> };
+    
+    # perhaps not happy about prolog?
+    $json =~ s/"\@encoding":"\S+?","\@version":"1.0",//;
     my $conf = XML::XML2JSON->new;
     my $xml  = $conf->json2xml($json);
+    my $fac  = $self->_factory;
+    my $proj = $fac->create_project;
+    my $ns   = $proj->get_namespaces;
+    for my $pre ( keys %{ $ns } ) {
+        my $uri = $ns->{$pre};
+        if ( $xml !~ /xmlns:$pre/ ) {
+            $xml =~ s/<nex:nexml /<nex:nexml xmlns:$pre="$uri" /;
+        }
+    }
     return @{ parse( '-format' => 'nexml', '-string' => $xml ) };
 }
 
