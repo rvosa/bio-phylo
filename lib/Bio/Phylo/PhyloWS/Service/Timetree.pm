d9b094f9f3b24a65059ae0e26f802a8898545399
rvosa <rutgeraldo@gmail.com>
2011-07-26 22:47:51 +0100

    Now only returns the weighted average age, and requires NCBI taxon IDs as terms, not taxon names.

--- lib/Bio/Phylo/PhyloWS/Service/Timetree.pm
+++ lib/Bio/Phylo/PhyloWS/Service/Timetree.pm
@@ -1,5 +1,6 @@
 package Bio::Phylo::PhyloWS::Service::Timetree;
 use strict;
+use utf8;
 use base 'Bio::Phylo::PhyloWS::Service';
 use Bio::Phylo::IO 'parse';
 use Bio::Phylo::Factory;
@@ -7,136 +8,54 @@ use Bio::Phylo::Util::Logger;
 use Bio::Phylo::Util::Exceptions 'throw';
 use Bio::Phylo::Util::CONSTANT qw'looks_like_hash looks_like_instance';
 use Bio::Phylo::Util::Dependency
-  qw'CQL::Parser LWP::UserAgent URI::Escape HTML::Tree HTML::Entities';
-use constant URL => 'http://timetree.org/time_e_query.php?';
+  qw'CQL::Parser LWP::UserAgent URI::Escape HTML::TreeBuilder::XPath HTML::Entities';
+use constant URL => 'http://timetree.org/time_query.php?';
 
 # http://localhost/nexml/service/timetree/phylows/tree/find?query=dcterms.title=Homo%20and%20dcterms.title=Pan
 {
-    my $pubmed =
-'http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&db=PubMed&list_uids=';
+    my $pubmed = 'http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&db=PubMed&list_uids=';
     my $fac    = Bio::Phylo::Factory->new;
     my $logger = Bio::Phylo::Util::Logger->new;
+    
     $logger->VERBOSE(
         '-level' => 4,
         '-class' => 'Bio::Phylo::Parsers::Timetree',
     );
-    my $process_tbody = sub {
-        my ( $tbody, $dates ) = @_;
-        my ( $pub, $taxona, $taxonb, $source, $data, $genes, $time );
-        for my $tr ( $tbody->content_list ) {
-            if ( looks_like_instance( $tr, 'HTML::Element' )
-                and $tr->tag eq 'tr' )
-            {
-
-                # a header row
-                if (    $tr->attr('bgcolor')
-                    and $tr->attr('bgcolor') eq '#FFFFFF' )
-                {
-                    for my $a ( $tr->descendents ) {
-                        if ( looks_like_instance( $a, 'HTML::Element' )
-                            and $a->tag eq 'a' )
-                        {
-                            my $href = $a->attr('href');
-                            if ( $href =~ /\Q$pubmed\E(\d+)/ ) {
-                                $pub = $1;
-                            }
-                        }
-                    }
-                }
-
-                # a content row
-                elsif ( $tr->attr('class') eq 'collapsible' ) {
-                    for my $div ( $tr->descendents ) {
-                        if ( looks_like_instance( $div, 'HTML::Element' )
-                            and $div->tag eq 'div' )
-                        {
-                            if ( $div->attr('class') eq 'mockTD' ) {
-                                if ( not defined $time ) {
-                                    $time = $div->as_text;
-                                }
-                                elsif ( not defined $genes ) {
-                                    $genes = $div->as_text;
-                                }
-                                elsif ( not defined $data ) {
-                                    $data = $div->as_text;
-                                }
-                            }
-                            elsif ( $div->attr('class') eq 'mockTDTaxa' ) {
-                                if ( not defined $taxona ) {
-                                    $taxona = $div->as_text;
-                                }
-                                elsif ( not defined $taxonb ) {
-                                    $taxonb = $div->as_text;
-                                }
-                            }
-                            elsif ( $div->attr('class') eq 'mockTDSource' ) {
-                                $source = $div->as_text;
-                                push @{$dates},
-                                  {
-                                    'pub'    => $pub,
-                                    'taxona' => $taxona,
-                                    'taxonb' => $taxonb,
-                                    'source' => $source,
-                                    'data'   => $data,
-                                    'genes'  => $genes,
-                                    'time'   => $time
-                                  };
-                                (
-                                    $taxona, $taxonb, $source,
-                                    $data,   $genes,  $time
-                                  )
-                                  = ( undef, undef, undef, undef, undef,
-                                    undef );
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    
+    my $parse_html = sub {
+        my ( $content ) = @_;
+        my $tree = HTML::TreeBuilder::XPath->new;
+        $tree->parse_content($content);
+        my ($title) = $tree->findnodes(q{//td[@class='titleBlack']/strong});
+        my $str = $title->as_text;
+        $str =~ s/^(\d+\.\d+).*$/$1/;
+        return $str;
     };
-    my $find_tbody = sub {
-        my ( $node, $dates ) = @_;
-        for my $table ( $node->descendents ) {
-            if ( looks_like_instance( $table, 'HTML::Element' ) ) {
-                my $class = $table->attr('class');
-                if (    $table->tag eq 'table'
-                    and $class
-                    and $class eq 'collapsible' )
-                {
-                    for my $tbody ( $table->content_list ) {
-                        if ( looks_like_instance( $tbody, 'HTML::Element' )
-                            and $tbody->tag =~ /tbody/i )
-                        {
-                            $process_tbody->( $tbody, $dates );
-                        }
-                    }
-                }
-            }
-        }
-    };
+    
     my $get_query_result_raw = sub {
         my ( $self, $taxon_a, $taxon_b ) = @_;
         my $lua = LWP::UserAgent->new;
-        my $response =
-          $lua->get( URL
+        my $url = URL
               . 'taxon_a='
               . URI::Escape::uri_escape($taxon_a) . '&'
               . 'taxon_b='
-              . URI::Escape::uri_escape($taxon_b) );
+              . URI::Escape::uri_escape($taxon_b);              
+        my $response = $lua->get($url);
         if ( $response->is_success ) {
-            return $response->content;
+            my $content = $response->content;
+            utf8::decode($content);
+            return $content;
         }
         else {
             throw 'NetworkError' => $response->status_line;
         }
     };
+    
     my $normalize_query = sub {
         my ( $node, $taxon_a_ref, $taxon_b_ref, $sub ) = @_;
         if ( looks_like_instance( $node, 'CQL::BooleanNode' ) ) {
-            $node->{'left'} =
-              $sub->( $node->{'left'}, $taxon_a_ref, $taxon_b_ref, $sub );
-            $node->{'right'} =
-              $sub->( $node->{'right'}, $taxon_a_ref, $taxon_b_ref, $sub );
+            $node->{'left'}  = $sub->( $node->{'left'}, $taxon_a_ref, $taxon_b_ref, $sub );
+            $node->{'right'} = $sub->( $node->{'right'}, $taxon_a_ref, $taxon_b_ref, $sub );
             return $node;
         }
         elsif ( looks_like_instance( $node, 'CQL::TermNode' ) ) {
@@ -149,35 +68,19 @@ use constant URL => 'http://timetree.org/time_e_query.php?';
         }
         return $node;
     };
+    
     my $create_project = sub {
+        my ($name_a,$name_b) = @_;
         my $project = $fac->create_project;
         my $forest  = $fac->create_forest;
-        my $taxa    = $fac->create_taxa;
-        my $taxona =
-          $fac->create_taxon( '-xml_id' => 'taxona', '-name' => 'taxon_a' );
-        my $taxonb =
-          $fac->create_taxon( '-xml_id' => 'taxonb', '-name' => 'taxon_b' );
+        my $taxa    = $fac->create_taxa;        
+        my $taxona  = $fac->create_taxon( '-name' => $name_a );        
+        my $taxonb  = $fac->create_taxon( '-name' => $name_b );
+        
         $project->insert( $taxa, $forest );
         $taxa->insert( $taxona, $taxonb );
         $forest->set_taxa($taxa);
         return ( $project, $taxona, $taxonb, $forest );
-    };
-    my $return_project = sub {
-        my $project = shift;
-        print "Content-type: text/xml\n\n";
-        print $project->to_xml;
-        exit(0);
-    };
-    my $create_node = sub {
-        my ( $date, $taxon_name, $field, $root ) = @_;
-        $fac->create_node(
-            '-branch_length' => $date->{'time'},
-            '-parent'        => $root,
-            '-taxon'         => $taxon_name,
-            '-name'          => $date->{$field} =~ m/\s/
-            ? "'" . $date->{$field} . "'"
-            : $date->{$field},
-        );
     };
 
 =head1 NAME
@@ -231,43 +134,29 @@ Gets a phylows cql query result
         my $content = $get_query_result_raw->( $self, $taxon_a, $taxon_b );
 
         # parse timetree result
-        my $tre = HTML::TreeBuilder->new;
-        $tre->parse($content);
-        $tre->eof;
-        my $dates = [];
-        $find_tbody->( $tre, $dates );
+        my $age = $parse_html->( $content );
 
         # populate project
-        my ( $project, $taxona, $taxonb, $forest ) = $create_project->();
-        $project->set_namespaces( 'tt' => 'http://timetree.org/terms#' );
-        for my $date ( @{$dates} ) {
-            my $tree = $fac->create_tree;
-            for my $field (qw(genes data source)) {
-                $tree->add_meta(
-                    $fac->create_meta(
-                        '-triple' => { "tt:$field" => $date->{$field} }
-                    )
-                );
-            }
-            $tree->add_meta(
-                $fac->create_meta(
-                    '-triple' => {
-                        "tt:pub" => HTML::Entities::encode_entities(
-                            $pubmed . $date->{'pub'} . '&dopt=Abstract'
-                        )
-                    }
-                )
-            );
-            my $root = $fac->create_node;
-            $tree->insert($root);
-            my $node_a = $create_node->( $date, $taxona, 'taxona', $root );
-            my $node_b = $create_node->( $date, $taxonb, 'taxonb', $root );
-            $tree->insert( $node_a, $node_b );
-            $forest->insert($tree);
-        }
+        my ( $project, $taxona, $taxonb, $forest ) = $create_project->($taxon_a,$taxon_b);
+        my $tree = $fac->create_tree;
+        my $tree_root = $fac->create_node;
+        
+        my $node_a = $fac->create_node(
+            '-branch_length' => $age,
+            '-parent'        => $tree_root,
+            '-taxon'         => $taxona,
+        );
+        
+        my $node_b = $fac->create_node(
+            '-branch_length' => $age,
+            '-parent'        => $tree_root,
+            '-taxon'         => $taxonb,
+        );
+        
+        $tree->insert( $tree_root, $node_a, $node_b );
+        $forest->insert($tree);
 
         # return project
-        # $return_project->($project);
         return $project;
     }
 
