f02201d3d42882016aa65affdf44eab33f657ae9
rvosa <rutgeraldo@gmail.com>
2011-08-23 18:34:59 +0900

    Changed to work on characters.rdf

--- experimental/rdf.pl
+++ experimental/rdf.pl
@@ -6,27 +6,21 @@ use RDF::Trine::Model;
 use Bio::Phylo::IO 'parse';
 use Bio::Phylo::Util::Logger ':levels';
 
-#my $logger = Bio::Phylo::Util::Logger->new(
-#    '-level' => DEBUG,
-#    '-class' => 'Bio::Phylo::Parsers::Cdao',
-#);
+my $logger = Bio::Phylo::Util::Logger->new(
+    '-level'  => DEBUG,
+    '-method' => 'Bio::Phylo::Parsers::Cdao::_process_nodes',
+);
 
 my $base   = 'http://example.org/';
 my $store  = RDF::Trine::Store::Memory->new;
 my $model  = RDF::Trine::Model->new( $store );
 my $parser = RDF::Trine::Parser->new('rdfxml');
 
-$parser->parse_file_into_model( $base, 'trees.rdf', $model );
-#my $node = RDF::Trine::Node::Resource->new($base . '#ne16');
-#
-#my $iterator = $model->get_statements($node);
-#while ( my $inner = $iterator->next ) {
-#    print $inner->subject, ' ', $inner->predicate, ' ', $inner->object, "\n";
-#}
+$parser->parse_file_into_model( $base, 'characters.rdf', $model );
 
 print parse(
     '-format' => 'cdao',
-    '-file'   => 'trees.rdf',
+    '-file'   => 'characters.rdf',
     '-model'  => $model,
     '-base'   => $base,
     '-as_project' => 1,
