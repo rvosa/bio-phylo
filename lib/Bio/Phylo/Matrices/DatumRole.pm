469973a9d0106c8bb6f9fcd3e2c86f1cc131d8f2
Rutger Vos <rutgeraldo@gmail.com>
2012-08-11 21:00:01 +0400

    All unit tests now pass for this separation of state and functionality

--- lib/Bio/Phylo/Matrices/DatumRole.pm
+++ lib/Bio/Phylo/Matrices/DatumRole.pm
@@ -12,7 +12,9 @@ my $LOADED_WRAPPERS = 0;
     my $logger             = __PACKAGE__->get_logger;
     my $TYPE_CONSTANT      = _DATUM_;
     my $CONTAINER_CONSTANT = _MATRIX_;
+    {
     my @fields             = \( my ( %weight, %position, %annotations ) );
+    }
 
 =head1 NAME
 
@@ -162,34 +164,6 @@ Datum constructor from Bio::Seq argument.
 
 =over
 
-=item set_weight()
-
-Sets invocant weight.
-
- Type    : Mutator
- Title   : set_weight
- Usage   : $datum->set_weight($weight);
- Function: Assigns a datum's weight.
- Returns : Modified object.
- Args    : The $weight argument must be a
-           number in any of Perl's number
-           formats.
-
-=cut
-
-    sub set_weight {
-        my ( $self, $weight ) = @_;
-        my $id = $self->get_id;
-        $weight = 1 if not defined $weight;
-        if ( looks_like_number $weight ) {
-            $weight{$id} = $weight;
-            $logger->info("setting weight '$weight'");
-        }
-        elsif ( !looks_like_number $weight ) {
-            throw 'BadNumber' => 'Not a number!';
-        }
-    }
-
 =item set_char()
 
 Sets character state(s)
@@ -251,149 +225,6 @@ Sets character state(s)
         return $self;
     }
 
-=item set_position()
-
-Set invocant starting position.
-
- Type    : Mutator
- Title   : set_position
- Usage   : $datum->set_position($pos);
- Function: Assigns a datum's position.
- Returns : Modified object.
- Args    : $pos must be an integer.
-
-=cut
-
-    sub set_position {
-        my ( $self, $pos ) = @_;
-        $pos = 1 if not defined $pos;
-        if ( looks_like_number $pos && $pos >= 1 && $pos / int($pos) == 1 ) {
-            $position{ $self->get_id } = $pos;
-            $logger->info("setting position '$pos'");
-        }
-        else {
-            throw 'BadNumber' => "'$pos' not a positive integer!";
-        }
-    }
-
-=item set_annotation()
-
-Sets single annotation.
-
- Type    : Mutator
- Title   : set_annotation
- Usage   : $datum->set_annotation(
-               -char       => 1,
-               -annotation => { -codonpos => 1 }
-           );
- Function: Assigns an annotation to a
-           character in the datum.
- Returns : Modified object.
- Args    : Required: -char       => $int
-           Optional: -annotation => $hashref
- Comments: Use this method to annotate
-           a single character. To annotate
-           multiple characters, use
-           'set_annotations' (see below).
-
-=cut
-
-    sub set_annotation {
-        my $self = shift;
-        if (@_) {
-            my %opt = looks_like_hash @_;
-            if ( not exists $opt{'-char'} ) {
-                throw 'BadArgs' => "No character to annotate specified!";
-            }
-            my $i   = $opt{'-char'};
-            my $id  = $self->get_id;
-            my $pos = $self->get_position;
-            my $len = $self->get_length;
-            if ( $i > ( $pos + $len ) || $i < $pos ) {
-                throw 'OutOfBounds' => "Specified char ($i) does not exist!";
-            }
-            if ( exists $opt{'-annotation'} ) {
-                my $note = $opt{'-annotation'};
-                $annotations{$id}->[$i] = {} if !$annotations{$id}->[$i];
-                while ( my ( $k, $v ) = each %{$note} ) {
-                    $annotations{$id}->[$i]->{$k} = $v;
-                }
-            }
-            else {
-                $annotations{$id}->[$i] = undef;
-            }
-        }
-        else {
-            throw 'BadArgs' => "No character to annotate specified!";
-        }
-        return $self;
-    }
-
-=item set_annotations()
-
-Sets list of annotations.
-
- Type    : Mutator
- Title   : set_annotations
- Usage   : $datum->set_annotations(
-               { '-codonpos' => 1 },
-               { '-codonpos' => 2 },
-               { '-codonpos' => 3 },
-           );
- Function: Assign annotations to
-           characters in the datum.
- Returns : Modified object.
- Args    : Hash references, where
-           position in the argument
-           list matches that of the
-           specified characters in
-           the character list. If no
-           argument given, annotations
-           are reset.
- Comments: Use this method to annotate
-           multiple characters. To
-           annotate a single character,
-           use 'set_annotation' (see
-           above).
-
-=cut
-
-    sub set_annotations {
-        my $self = shift;
-        my @anno;
-        if ( scalar @_ == 1 and looks_like_instance( $_[0], 'ARRAY' ) ) {
-            @anno = @{ $_[0] };
-        }
-        else {
-            @anno = @_;
-        }
-        my $id = $self->get_id;
-        if (@anno) {
-            my $max_index = $self->get_length - 1;
-            for my $i ( 0 .. $#anno ) {
-                if ( $i > $max_index ) {
-                    throw 'OutOfBounds' =>
-                      "Specified char ($i) does not exist!";
-                }
-                else {
-                    if ( looks_like_instance( $anno[$i], 'HASH' ) ) {
-                        $annotations{$id}->[$i] = {}
-                          if !$annotations{$id}->[$i];
-                        while ( my ( $k, $v ) = each %{ $anno[$i] } ) {
-                            $annotations{$id}->[$i]->{$k} = $v;
-                        }
-                    }
-                    else {
-                        next;
-                    }
-                }
-            }
-        }
-        else {
-            $annotations{$id} = [];
-        }
-    }
-
 =back
 
 =head2 ACCESSORS
@@ -414,25 +245,6 @@ Gets the matrix (if any) this datum belongs to
 =cut
 
     sub get_matrix { shift->_get_container }
-
-=item get_weight()
-
-Gets invocant weight.
-
- Type    : Accessor
- Title   : get_weight
- Usage   : my $weight = $datum->get_weight;
- Function: Retrieves a datum's weight.
- Returns : FLOAT
- Args    : NONE
-
-=cut
-
-    sub get_weight {
-        my $self   = shift;
-        my $weight = $weight{ $self->get_id };
-        return defined $weight ? $weight : 1;
-    }
 
 =item get_char()
 
@@ -493,88 +305,6 @@ Gets unaligned characters, i.e. without gap or missing symbols
         else {
             return wantarray ? () : '';
         }        
-    }
-
-=item get_position()
-
-Gets invocant starting position.
-
- Type    : Accessor
- Title   : get_position
- Usage   : my $pos = $datum->get_position;
- Function: Retrieves a datum's position.
- Returns : a SCALAR integer.
- Args    : NONE
-
-=cut
-
-    sub get_position {
-        my $self = shift;
-        my $pos  = $position{ $self->get_id };
-        return defined $pos ? $pos : 1;
-    }
-
-=item get_annotation()
-
-Retrieves character annotation (hashref).
-
- Type    : Accessor
- Title   : get_annotation
- Usage   : $datum->get_annotation(
-               '-char' => 1,
-               '-key'  => '-codonpos',
-           );
- Function: Retrieves an annotation to
-           a character in the datum.
- Returns : SCALAR or HASH
- Args    : Optional: -char => $int
-           Optional: -key => $key
-
-=cut
-
-    sub get_annotation {
-        my $self = shift;
-        my $id   = $self->get_id;
-        if (@_) {
-            my %opt = looks_like_hash @_;
-            if ( not exists $opt{'-char'} ) {
-                throw 'BadArgs' =>
-                  "No character to return annotation for specified!";
-            }
-            my $i   = $opt{'-char'};
-            my $pos = $self->get_position;
-            my $len = $self->get_length;
-            if ( $i < $pos || $i > ( $pos + $len ) ) {
-                throw 'OutOfBounds' => "Specified char ($i) does not exist!";
-            }
-            if ( exists $opt{'-key'} ) {
-                return $annotations{$id}->[$i]->{ $opt{'-key'} };
-            }
-            else {
-                return $annotations{$id}->[$i];
-            }
-        }
-        else {
-            return $annotations{$id};
-        }
-    }
-
-=item get_annotations()
-
-Retrieves character annotations (array ref).
-
- Type    : Accessor
- Title   : get_annotations
- Usage   : my @anno = @{ $datum->get_annotation() };
- Function: Retrieves annotations
- Returns : ARRAY
- Args    : NONE
-
-=cut
-
-    sub get_annotations {
-        my $self = shift;
-        return $annotations{ $self->get_id } || [];
     }
 
 =item get_length()
@@ -1194,16 +924,6 @@ Analog to to_xml.
     sub _type      { $TYPE_CONSTANT }
     sub _container { $CONTAINER_CONSTANT }
     sub _tag       { 'row' }
-
-    sub _cleanup {
-        my $self = shift;
-        $logger->info("cleaning up '$self'");
-        if ( defined( my $id = $self->get_id ) ) {
-            for my $field (@fields) {
-                delete $field->{$id};
-            }
-        }
-    }
     
     sub _update_characters {
         my $self = shift;
