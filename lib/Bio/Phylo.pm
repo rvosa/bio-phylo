3365c4644c6da488d123787ed4565387985f4fd8
rvosa <rutgeraldo@gmail.com>
2013-04-11 17:07:54 +0200

    incrementing version number for CPAN release

--- lib/Bio/Phylo.pm
+++ lib/Bio/Phylo.pm
@@ -17,7 +17,7 @@ use Bio::Phylo::Util::Logger;             # for logging, like log4perl/log4j
 use Bio::Phylo::Identifiable;    # for storing unique IDs inside an instance
 
 our ( $logger, $COMPAT ) = Bio::Phylo::Util::Logger->new;
-our $VERSION = "0.52";
+our $VERSION = "0.53";
 
 # mediates one-to-many relationships between taxon and nodes,
 # taxon and sequences, taxa and forests, taxa and matrices.
