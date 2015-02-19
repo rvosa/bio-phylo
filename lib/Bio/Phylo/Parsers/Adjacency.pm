595bd01856597795a26794d08c873101b93e14af
rvosa <rutgeraldo@gmail.com>
2015-02-19 13:54:45 +0100

    added progress message

--- lib/Bio/Phylo/Parsers/Adjacency.pm
+++ lib/Bio/Phylo/Parsers/Adjacency.pm
@@ -45,6 +45,9 @@ sub _parse {
 	my ( @header, %node_cols );
 	my %node_for_id;
     LINE: while (<$fh>) {
+    	unless ( scalar(keys(%node_for_id)) % 1000 ) {
+    		$log->debug("processed node " . scalar(keys(%node_for_id)));
+    	}
         chomp;
 		
 		# the first line is the header row
