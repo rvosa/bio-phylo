23fa1791d11659585a5aa6936a9a407518e00ffe
Rutger Vos <rutgeraldo@gmail.com>
2017-10-19 16:11:50 +0200

    Update Cdao.pm

--- lib/Bio/Phylo/Parsers/Cdao.pm
+++ lib/Bio/Phylo/Parsers/Cdao.pm
@@ -66,7 +66,7 @@ WHERE {
 STATES
 
 sub _parse {
-    warn "***Please note: implementation is incomplete, character data are not yet read correctly.\n***";
+    warn "***Please note: implementation is incomplete, character data are not yet read correctly.***\n";
     my $self = shift;
     %objects = ();
     $self->_args->{'-opts'} = {
