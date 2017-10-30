49e30bb0c9132661e01a9b0111860eb1a6da5e21
Rutger Vos <rutgeraldo@gmail.com>
2017-10-30 20:35:30 +0100

    incrementing version number

--- lib/Bio/Phylo.pm
+++ lib/Bio/Phylo.pm
@@ -14,7 +14,7 @@ use Bio::Phylo::Util::MOP;                # for traversing inheritance trees
 use Bio::Phylo::Identifiable;             # for storing unique IDs inside an instance
 
 our ( $logger, $COMPAT ) = Bio::Phylo::Util::Logger->new;
-use version 0.77; our $VERSION = qv("v2.0.0");
+use version 0.77; our $VERSION = qv("v2.0.1");
 
 # mediates one-to-many relationships between taxon and nodes,
 # taxon and sequences, taxa and forests, taxa and matrices.
